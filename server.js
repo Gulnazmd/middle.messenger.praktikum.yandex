de988369025aa930a21f52f8e2adc65f85337ee2
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2022-05-31 10:55:04 +0300

    default port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static('./'));
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(`Example app listening on port ${process.env.PORT}!`);
 });
