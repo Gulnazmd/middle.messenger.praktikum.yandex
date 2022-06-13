0b6d0730a7e9fa62fa8a874546cc736539a20ec5
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2022-06-13 08:40:59 +0300

    fix

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const express = require('express');
 
 const app = express();
 
-const path = require('path');
-
-app.use(express.static('./dist'));
-
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + './dist/index.html');
-});
+app.use(express.static('./'));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(`Example app listening on port ${process.env.PORT}!`);
